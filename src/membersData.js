4306c1dd8f773cd29337e8e45374972224c5ffe3
Michelle P. Kuchera <mikuchera@davidson.edu>
2025-05-14 01:20:59 -0400

    Update membersData.js

--- src/membersData.js
+++ src/membersData.js
@@ -216,15 +216,14 @@ const membersData = {
         "Ryan Strauss": ["Point Cloud Generation", "Downstream Tasks", "ATTPC"],
         "Jack Taylor": ["Classification", "ATTPC"],
         "Alex Karbo": ["SUSY BNN"]
-    }    
-  };
+    },    
 
-const memberLinks = {
+memberLinks: {
   "Michelle Kuchera": "https://www.linkedin.com/in/michellekuchera/",
   "Raghu Ramanujan": "https://www.linkedin.com/in/raghuramanujan/",
   "Ann Zhu": "https://www.linkedin.com/in/ann-zhuqianhui/",
   "Ben Votaw": "https://www.linkedin.com/in/ben-votaw-7241b4237/",
   // Add more as available
-};
-  
-export { membersData, memberLinks };
+}
+  };  
+export default membersData;
